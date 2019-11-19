6d9a4d9cd386942c8d2f452f542e0e906648a239
nhishon <nhishon@luc.edu>
2019-11-19 16:46:23 -0600

    orderline product

--- flaskDemo/models.py
+++ flaskDemo/models.py
@@ -18,7 +18,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    # posts = db.relationship('Post', backref='author', lazy=True)
+    orders = db.relationship('Order', backref='userOrder', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -43,9 +43,40 @@ class Order(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     # content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    orderLines = db.relationship('OrderLine', backref='orderOrderLine', lazy=True)
 
     def __repr__(self):
         return f"Order('{self.date_posted}')"
+
+class OrderLine(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    # title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # content = db.Column(db.Text, nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    def __repr__(self):
+        return f"OrderLine('{self.date_posted}')"
+
+
+class Product(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # content = db.Column(db.Text, nullable=False)
+    orderLines = db.relationship('OrderLine', backref='product  OrderLine', lazy=True)
+
+
+    def __repr__(self):
+        return f"Product('{self.title}', '{self.date_posted}')"
+
+# orderline
+
+
 
 
 
