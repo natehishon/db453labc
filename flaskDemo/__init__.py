a1224e4833a3d8213d5a0c83e5a9f0645c6c896a
nhishon <nhishon@luc.edu>
2019-11-19 14:11:51 -0600

    moved bad react code, implemented orders

--- flaskDemo/__init__.py
+++ flaskDemo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:8889/company'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:8889/project'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -3,8 +3,8 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskDemo import app, db, bcrypt
-from flaskDemo.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, DeptForm,DeptUpdateForm
-from flaskDemo.models import User, Post,Department, Dependent, Dept_Locations, Employee, Project, Works_On
+from flaskDemo.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskDemo.models import User, Order
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
 
@@ -12,25 +12,25 @@ from datetime import datetime
 @app.route("/")
 @app.route("/home")
 def home():
-    results = Department.query.all()
-    return render_template('dept_home.html', outString = results)
-    posts = Post.query.all()
-    return render_template('home.html', posts=posts)
-    results2 = Faculty.query.join(Qualified,Faculty.facultyID == Qualified.facultyID) \
-               .add_columns(Faculty.facultyID, Faculty.facultyName, Qualified.Datequalified, Qualified.courseID) \
-               .join(Course, Course.courseID == Qualified.courseID).add_columns(Course.courseName)
-    results = Faculty.query.join(Qualified,Faculty.facultyID == Qualified.facultyID) \
-              .add_columns(Faculty.facultyID, Faculty.facultyName, Qualified.Datequalified, Qualified.courseID)
-    return render_template('join.html', title='Join',joined_1_n=results, joined_m_n=results2)
+    results = Order.query.all()
+    # return render_template('dept_home.html', outString = results)
+    # posts = Post.query.all()
+    # return render_template('home.html', posts=posts)
+    # results2 = Faculty.query.join(Qualified,Faculty.facultyID == Qualified.facultyID) \
+    #            .add_columns(Faculty.facultyID, Faculty.facultyName, Qualified.Datequalified, Qualified.courseID) \
+    #            .join(Course, Course.courseID == Qualified.courseID).add_columns(Course.courseName)
+    # results = Faculty.query.join(Qualified,Faculty.facultyID == Qualified.facultyID) \
+    #           .add_columns(Faculty.facultyID, Faculty.facultyName, Qualified.Datequalified, Qualified.courseID)
+    return render_template('orders.html', title='Orders',orders=results)
 
    
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-
+# @app.route("/about")
+# def about():
+#     return render_template('about.html', title='About')
+#
+#
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -44,8 +44,8 @@ def register():
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-
-
+#
+#
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -60,27 +60,27 @@ def login():
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
+#
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
-
-
-def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-
-    output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-
-    return picture_fn
-
+#
+#
+# def save_picture(form_picture):
+#     random_hex = secrets.token_hex(8)
+#     _, f_ext = os.path.splitext(form_picture.filename)
+#     picture_fn = random_hex + f_ext
+#     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+#
+#     output_size = (125, 125)
+#     i = Image.open(form_picture)
+#     i.thumbnail(output_size)
+#     i.save(picture_path)
+#
+#     return picture_fn
+#
 
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
@@ -101,143 +101,143 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
-
-
-@app.route("/dept/new", methods=['GET', 'POST'])
-@login_required
-def new_dept():
-    form = DeptForm()
-    if form.validate_on_submit():
-        dept = Department(dname=form.dname.data, dnumber=form.dnumber.data,mgr_ssn=form.mgr_ssn.data,mgr_start=form.mgr_start.data)
-        db.session.add(dept)
-        db.session.commit()
-        flash('You have added a new department!', 'success')
-        return redirect(url_for('home'))
-    return render_template('create_dept.html', title='New Department',
-                           form=form, legend='New Department')
-
-
-@app.route("/dept/<dnumber>")
-@login_required
-def dept(dnumber):
-    dept = Department.query.get_or_404(dnumber)
-    return render_template('dept.html', title=dept.dname, dept=dept, now=datetime.utcnow())
-
-
-@app.route("/dept/<dnumber>/update", methods=['GET', 'POST'])
-@login_required
-def update_dept(dnumber):
-    dept = Department.query.get_or_404(dnumber)
-    currentDept = dept.dname
-
-    form = DeptUpdateForm()
-    if form.validate_on_submit():          # notice we are are not passing the dnumber from the form
-        if currentDept !=form.dname.data:
-            dept.dname=form.dname.data
-        dept.mgr_ssn=form.mgr_ssn.data
-        dept.mgr_start=form.mgr_start.data
-        db.session.commit()
-        flash('Your department has been updated!', 'success')
-        return redirect(url_for('dept', dnumber=dnumber))
-    elif request.method == 'GET':             
-        form.dnumber.data = dept.dnumber   # notice that we ARE passing the dnumber to the form
-        form.dname.data = dept.dname
-        form.mgr_ssn.data = dept.mgr_ssn
-        form.mgr_start.data = dept.mgr_start
-    return render_template('update_dept.html', title='Update Department',
-                           form=form, legend='Update Department')          # note the update template!
-
-
-
-
-@app.route("/dept/<dnumber>/delete", methods=['POST'])
-@login_required
-def delete_dept(dnumber):
-    dept = Department.query.get_or_404(dnumber)
-    db.session.delete(dept)
-    db.session.commit()
-    flash('The department has been deleted!', 'success')
-    return redirect(url_for('home'))
-
-
-
-# @app.route("/remove-employee-proj/delete", methods=['POST'])
+#
+#
+# @app.route("/dept/new", methods=['GET', 'POST'])
 # @login_required
-# def delete_employee():
-#     req_data = request.get_json()
-#     print(req_data);
-#     # dept = Department.query.get_or_404(dnumber)
-#     # db.session.delete(dept)
-#     # db.session.commit()
+# def new_dept():
+#     form = DeptForm()
+#     if form.validate_on_submit():
+#         dept = Department(dname=form.dname.data, dnumber=form.dnumber.data,mgr_ssn=form.mgr_ssn.data,mgr_start=form.mgr_start.data)
+#         db.session.add(dept)
+#         db.session.commit()
+#         flash('You have added a new department!', 'success')
+#         return redirect(url_for('home'))
+#     return render_template('create_dept.html', title='New Department',
+#                            form=form, legend='New Department')
+#
+#
+# @app.route("/dept/<dnumber>")
+# @login_required
+# def dept(dnumber):
+#     dept = Department.query.get_or_404(dnumber)
+#     return render_template('dept.html', title=dept.dname, dept=dept, now=datetime.utcnow())
+#
+#
+# @app.route("/dept/<dnumber>/update", methods=['GET', 'POST'])
+# @login_required
+# def update_dept(dnumber):
+#     dept = Department.query.get_or_404(dnumber)
+#     currentDept = dept.dname
+#
+#     form = DeptUpdateForm()
+#     if form.validate_on_submit():          # notice we are are not passing the dnumber from the form
+#         if currentDept !=form.dname.data:
+#             dept.dname=form.dname.data
+#         dept.mgr_ssn=form.mgr_ssn.data
+#         dept.mgr_start=form.mgr_start.data
+#         db.session.commit()
+#         flash('Your department has been updated!', 'success')
+#         return redirect(url_for('dept', dnumber=dnumber))
+#     elif request.method == 'GET':
+#         form.dnumber.data = dept.dnumber   # notice that we ARE passing the dnumber to the form
+#         form.dname.data = dept.dname
+#         form.mgr_ssn.data = dept.mgr_ssn
+#         form.mgr_start.data = dept.mgr_start
+#     return render_template('update_dept.html', title='Update Department',
+#                            form=form, legend='Update Department')          # note the update template!
+#
+#
+#
+#
+# @app.route("/dept/<dnumber>/delete", methods=['POST'])
+# @login_required
+# def delete_dept(dnumber):
+#     dept = Department.query.get_or_404(dnumber)
+#     db.session.delete(dept)
+#     db.session.commit()
 #     flash('The department has been deleted!', 'success')
 #     return redirect(url_for('home'))
-
-
-
-
-@app.route("/employees")
-def employees():
-    employeeProjects = Employee.query.join(Works_On, Employee.ssn == Works_On.essn) \
-        .add_columns(Employee.ssn, Employee.fname, Employee.lname, Works_On.essn, Works_On.pno) \
-        .join(Project, Project.pnumber == Works_On.pno).add_columns(Project.pname)
-
-    return render_template('assign_home.html', title='Employees', joined_m_n=employeeProjects)
-
-@app.route("/projects")
-def projects():
-    projects = Project.query.all()
-
-    return render_template('projects.html', title='Projects', joined_m_n=projects)
-
-
-
-@app.route("/assign1")
-@login_required
-def assign1():
-    essn = request.args.get('essn')
-    pno = request.args.get('pno')
-    assign = Works_On.query.get_or_404([essn,pno])
-    return render_template('assign.html', title=str(assign.essn)+"_"+str(assign.pno), assign=assign,now=datetime.utcnow())
-
-
-@app.route("/assign/<essn>/<pno>delete", methods=['POST'])
-@login_required
-def delete_assign(essn,pno):
-    assign = Works_On.query.get_or_404([essn,pno])
-    db.session.delete(assign)
-    db.session.commit()
-    flash('The works on has been deleted!', 'success')
-    return redirect(url_for('home'))
-
-
-@app.route("/project/<pnumber>")
-@login_required
-def project(pnumber):
-
-    project = Project.query.get_or_404(pnumber)
-
-    # project = sqlraw(select * from
-
-    currentEmployees = Employee.query.join(Works_On, Employee.ssn == Works_On.essn) \
-        .add_columns(Employee.ssn, Employee.fname, Employee.lname, Works_On.essn, Works_On.pno) \
-        .join(Project, Project.pnumber == Works_On.pno).add_columns(Project.pname).filter(Project.pnumber == pnumber)
-    #
-
-    currentIds = db.session.query(Employee.ssn). \
-        join(Works_On, Works_On.essn == Employee.ssn). \
-        filter(Works_On.pno == pnumber)
-
-    availableEmployees = Employee.query.filter(Employee.ssn.notin_(currentIds)).all()
-
-
-
-    return render_template('project.html', title=project.pname, project=project, employees=currentEmployees, currentIds = availableEmployees, now=datetime.utcnow())
-
-
-@app.route("/works/<essn>/<pno>works", methods=['GET'])
-@login_required
-def new_works(essn,pno):
-    new_works = Works_On(essn=essn, pno=pno, hours=0)
-    db.session.add(new_works)
-    db.session.commit()
-    return redirect(url_for('projects'))
+#
+#
+#
+# # @app.route("/remove-employee-proj/delete", methods=['POST'])
+# # @login_required
+# # def delete_employee():
+# #     req_data = request.get_json()
+# #     print(req_data);
+# #     # dept = Department.query.get_or_404(dnumber)
+# #     # db.session.delete(dept)
+# #     # db.session.commit()
+# #     flash('The department has been deleted!', 'success')
+# #     return redirect(url_for('home'))
+#
+#
+#
+#
+# @app.route("/employees")
+# def employees():
+#     employeeProjects = Employee.query.join(Works_On, Employee.ssn == Works_On.essn) \
+#         .add_columns(Employee.ssn, Employee.fname, Employee.lname, Works_On.essn, Works_On.pno) \
+#         .join(Project, Project.pnumber == Works_On.pno).add_columns(Project.pname)
+#
+#     return render_template('assign_home.html', title='Employees', joined_m_n=employeeProjects)
+#
+# @app.route("/projects")
+# def projects():
+#     projects = Project.query.all()
+#
+#     return render_template('projects.html', title='Projects', joined_m_n=projects)
+#
+#
+#
+# @app.route("/assign1")
+# @login_required
+# def assign1():
+#     essn = request.args.get('essn')
+#     pno = request.args.get('pno')
+#     assign = Works_On.query.get_or_404([essn,pno])
+#     return render_template('assign.html', title=str(assign.essn)+"_"+str(assign.pno), assign=assign,now=datetime.utcnow())
+#
+#
+# @app.route("/assign/<essn>/<pno>delete", methods=['POST'])
+# @login_required
+# def delete_assign(essn,pno):
+#     assign = Works_On.query.get_or_404([essn,pno])
+#     db.session.delete(assign)
+#     db.session.commit()
+#     flash('The works on has been deleted!', 'success')
+#     return redirect(url_for('home'))
+#
+#
+# @app.route("/project/<pnumber>")
+# @login_required
+# def project(pnumber):
+#
+#     project = Project.query.get_or_404(pnumber)
+#
+#     # project = sqlraw(select * from
+#
+#     currentEmployees = Employee.query.join(Works_On, Employee.ssn == Works_On.essn) \
+#         .add_columns(Employee.ssn, Employee.fname, Employee.lname, Works_On.essn, Works_On.pno) \
+#         .join(Project, Project.pnumber == Works_On.pno).add_columns(Project.pname).filter(Project.pnumber == pnumber)
+#     #
+#
+#     currentIds = db.session.query(Employee.ssn). \
+#         join(Works_On, Works_On.essn == Employee.ssn). \
+#         filter(Works_On.pno == pnumber)
+#
+#     availableEmployees = Employee.query.filter(Employee.ssn.notin_(currentIds)).all()
+#
+#
+#
+#     return render_template('project.html', title=project.pname, project=project, employees=currentEmployees, currentIds = availableEmployees, now=datetime.utcnow())
+#
+#
+# @app.route("/works/<essn>/<pno>works", methods=['GET'])
+# @login_required
+# def new_works(essn,pno):
+#     new_works = Works_On(essn=essn, pno=pno, hours=0)
+#     db.session.add(new_works)
+#     db.session.commit()
+#     return redirect(url_for('projects'))
